d47712a9aa1f7f6879c3094c3599e65feaecf56e
Bomel <keiwanjamaly@hotmail.de>
2017-12-23 16:23:28 +0100

    save ex2

--- exersize 2/ex2.c
+++ exersize 2/ex2.c
@@ -3,6 +3,12 @@
 #include <math.h>
 
 
+float funtion(x,v){
+	return 
+}
+
+
+
 int main(void){		
 	float h = 0.05; //stepsize
 	float a = 1; //short for alpha
@@ -11,16 +17,21 @@ int main(void){
 	float t_0 = 0;
 	float T = 10;
 
-	float x_0 = 1; //starting position
+	float x_0 = 0; //starting parameters
+	float v_0 = 1
 
 	int steps = (T - t_0)/h; //Nuber of steps needed
 	double t[steps];
 	double x[steps];
+	double v[steps];
 	
 	t[0] = t_0;
+	v[0] = v_0;
 	x[0] = x_0;
 
 	for (int i = 1; i <= (steps -1); i++){
+		k1 = h*v[i-1]
+		w2 = h*
 		t[i] = t[i-1] + h;
 		x[i] = x[i-1] - a * x[i-1] * h;
 	}
