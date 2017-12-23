9d5f2a756cd360c6149109bf8618904fc925086f
Bastian Krones <bkrones.uni@gmail.com>
2017-12-23 16:51:21 +0100

    Merged

--- exercise_1/ex1.c
+++ exercise_1/ex1.c
@@ -2,41 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 
+int main(void)
+{
+	//stepsize
+	float h = 0.05;
 
-int main(void){		
-	float h = 0.05; //stepsize
-	float a = 1; //short for alpha
-
+	//short for alpha
+	float a = 1;
 	//define the intervall
 	float t_0 = 0;
-	float T = 10;
+	float T = 20;
 
-	float x_0 = 1; //starting position
+	//starting position
+	float x_0 = 1;
 
-	int steps = (T - t_0)/h; //Nuber of steps needed
+	//Nuber of steps needed
+	int steps = (T - t_0) / h;
 	double t[steps];
 	double x[steps];
-	
+
 	t[0] = t_0;
 	x[0] = x_0;
 
-	for (int i = 1; i <= (steps -1); i++){
-		t[i] = t[i-1] + h;
-		x[i] = x[i-1] - a * x[i-1] * h;
+	for (int i = 1; i <= (steps - 1); i++)
+	{
+		t[i] = t[i - 1] + h;
+		x[i] = x[i - 1] - a * x[i - 1] * h;
 	}
-	
 
-	FILE*fp;
+	FILE *fp;
 
-	fp = fopen("./data.txt","w");
-	
+	fp = fopen("./data.txt", "w");
 
-	for (int i = 0; i <= (steps -1); i++){
+	for (int i = 0; i <= (steps - 1); i++)
+	{
 		fprintf(fp, "%f %11f \n", t[i], x[i]);
 	}
 
 	fclose(fp);
-	
+
 	return 0;
-	
 }
