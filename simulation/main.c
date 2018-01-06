a9f9bf4faf99de5d568531f5c725f7f2046d94fa
Bomel <keiwanjamaly@hotmail.de>
2018-01-06 18:03:42 +0100

    he said i should push

--- simulation/main.c
+++ simulation/main.c
@@ -4,30 +4,64 @@
 
 #define N 3
 #define D 2
+#define g 6.67408 * pow(10,-11)
+#define S //need to calculated before
+//set inertial condition in form of xIJ where I is the I-th body and J the J-th dimention
+//set inertial condition in form of vIJ where I is the I-th body and J the J-th dimention
 
-double fInternal(int i, int j, int d, double t
+//the force applied on the i-th body due to the j-th body
+double fInternal(int i, int j, int d, int step, double x[N][D][S], double m[N]) {
+	if i==j {
+		return 0;
+	}
+	else {
+		double G = g;
+		//maybe change negative sign
+		return -G * (m[i]*m[j])/(pow(x[i][0][step] - x[j][0][step],2) + pow(x[i][1][step] - x[j][1][step],2) + pow(x[i][2][step] - x[j][2][step],2))	
+	}
+}
 
+//all the forces acting on the j-th body
+double fExternal(int i, int d, int step, double x[N][D][S], double m[N]){
+	double G = g;
+	double radius = 0;
+	for (int k = 0, k < N, k++){
+		radius += pow(x[i][k][step], 2);
+	}
+	double force = -G * (m[0]*m[i])/radius
+	for (int j = 1, j < N, j++){
+		force += fInternal(i, j, d, step, x, m)
+	}
 
+}
 
+//calculate the energy
+double calculate_energy(double x10, double x11, double x20, double x22, ){
+	double E = 
+}
 
 int main(void) {
 	//inertial parameters (position, velocity, time of simulation)
-	double X_0;
-	double V_0;
-	double T_0; 
+	double T_0 = 0; 
 	
 	//final time of simulation
-	double T_1; 
+	double T_1 = 10; 
 	
 	//stepsize of simulation
-	double h;
-
-	//amount of steps
-	int S = (T_1 - T_0)/h; 
-
+	double h = 0.1;
+		
 	typedef struct {
-		short energy_check;
+		double energy[S];
 		double prec;
 		double x[N][D][S], v[N][D][S], t[S];
+		double m[N];
 	} RungeKutta4;
+	
+	RungeKutta4 rg = {
+		.energy[0] = 
+	};	
+
+
+
+	return 0;
 }
