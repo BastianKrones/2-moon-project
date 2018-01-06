cd1954125b5f28305006398b287c5ba64a2f7d61
Bomel <keiwanjamaly@hotmail.de>
2018-01-06 19:52:47 +0100

    walalalala

--- simulation/main.c
+++ simulation/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include "initial_data.h"
+#include "initial_data.c"
 
 //the force applied on the i-th body due to the j-th body at the d-th dimention
 double fInternal(int i, int j, int d, int step, double x[N][D][S], double m[N]) {
@@ -22,10 +22,15 @@ double fInternal(int i, int j, int d, int step, double x[N][D][S], double m[N]) 
 //all the forces acting on the j-th body at the d-th dimention
 double fExternal(int i, int d, int step, double x[N][D][S], double m[N]){
 	//maybe change negative sign here too
-	double force = -G * (m[0]*m[i])/pow(pow(x[i][0][step],2) + pow(x[i][1][step],2) + pow(x[i][2][step],2) ,3/2) * x[i][d][step];
+	double force = -G * (m[0]*m[i])/pow(pow(x[i][0][step],2) 
+			+ pow(x[i][1][step],2) 
+			+ pow(x[i][2][step],2) ,3/2) 
+			* x[i][d][step];
+
 	for (int j = 1; j < N; j++){
 		force += fInternal(i, j, d, step, x, m);
 	}
+
 	return force;
 }
 
@@ -40,20 +45,16 @@ int main(void) {
 	//stepsize of simulation
 	double h = 0.1;
 		
-	typedef struct {
-		double energy[S];
-		double prec;
-		double x[N][D][S], v[N][D][S], t[S];
-		double m[N];
-	} RungeKutta4;
+	double energy[S];
+	double prec;
+	double x[N][D][S], v[N][D][S], t[S];
+	double m[N];
 	
 
 	for (int b = 0; b<N; b++){
-		for (int z = 0; z<D; z++){	
-			RungeKutta4 rg = {
-				.x[b][z][0] = initial_positions[b][z];
-				.v[b][z][0] = initial_velocities[b][z];
-			};	
+		for (int z = 0; z<D; z++){
+			x[b][z][0] = initial_positions[b][z];
+			v[b][z][0] = initial_velocities[b][z];
 		}
 	}
 
