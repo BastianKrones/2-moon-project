23564b55f1fc5fd512b60325d223a77fab1de559
Bastian Krones <bkrones.uni@gmail.com>
2018-01-06 18:35:54 +0100

    Blah

--- simulation/initial_data.c
+++ simulation/initial_data.c
@@ -1,18 +1,17 @@
 // Starttime
-const double = t_0 0
+const double = t_0 0;
 
 // Endtime
-const double = t_1 1
+const double = t_1 1;
 
 // Amount of bodies
-const int N = 3
+const int N = 3;
 
 // Space dimensions
-const int D = 2
+const int D = 2;
 
 // Gravitational constant
-const double G = 6.67408 * pow(10,-11)
+const double G = 6.67408 * pow(10, -11);
 
 // Steps of simulation | needs to be calculated individually
-const int S = 20
-
+const int S = 20;
