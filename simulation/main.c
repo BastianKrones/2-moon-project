5eb6f0c4237096ed0b27e97770a94d9df4519a9d
Bomel <keiwanjamaly@hotmail.de>
2018-01-14 17:19:09 +0100

    implementet next function

--- simulation/main.c
+++ simulation/main.c
@@ -72,7 +72,13 @@ double calculate_acceleration(int i, int d, int step, double x[N][D][S], double 
 
 //rungekutta shit lol :)
 double Next(double h, int step, double t[S], double x[N][D][S], double v[N][D][S]){
+	
+	//these k and w variables
 	double k[N][D][3], w[N][D][3];
+
+	double vprime[N][D][S] = v;
+       	double xprimt[N][D][S] = x;
+
 
 	//calculate k1 and w1
 	for (int i = 1; i<N; i++){
@@ -82,17 +88,60 @@ double Next(double h, int step, double t[S], double x[N][D][S], double v[N][D][S
 		}
 	}
 
-	double vprime[N][D], xprimt[N][D];
-	for (int i = 1; i<N; i++){
-		for (int j = 0; j<D; j++){
-
-		}
-	}
 	//calculate k2 and w2
 	for (int i = 1; i<N; i++){
-			w[i][j][1] = h * (v[i][j][step] + w[i][j][0]/2);
-			w[i][j][2] = h * calcualte_acceleration(i, j, step, x, m);
+		for (int j = 0; j<D; j++){
+			vprime[i][j][step] += w[i][j][0]/2;
+			xprime[i][j][step] += k[i][j][0]/2;
+		}
 	}
+
+	for (int i = 1; i<N; i++){
+		for (int j = 1; j<D; j++){
+			w[i][j][1] = h * (vprime[i][j][step]);
+			w[i][j][1] = h * calcualte_acceleration(i, j, step, xprime, m);
+		}
+	}
+
+	//calculate k3 and w3
+	for (int i = 1; i<N; i++){
+		for (int j = 0; j<D; j++){
+			vprime[i][j][step] += w[i][j][1]/2;
+			xprime[i][j][step] += k[i][j][1]/2;
+		}
+	}
+
+	for (int i = 1; i<N; i++){
+		for (int j = 1; j<D; j++){
+			w[i][j][2] = h * (vprime[i][j][step]);
+			w[i][j][2] = h * calcualte_acceleration(i, j, step, xprime, m);
+		}
+	}
+
+	//calculate k4 and w4
+	for (int i = 1; i<N; i++){
+		for (int j = 0; j<D; j++){
+			vprime[i][j][step] += w[i][j][2];
+			xprime[i][j][step] += k[i][j][2];
+		}
+	}
+
+	for (int i = 1; i<N; i++){
+		for (int j = 1; j<D; j++){
+			w[i][j][3] = h * (vprime[i][j][step]);
+			w[i][j][3] = h * calcualte_acceleration(i, j, step, xprime, m);
+		}
+	}
+
+	//calculate the new variables	
+	for (int i = 1; i<N; i++){
+		for (int j = 1; j<D; j++){
+			x[i][j][step + 1] = x[i][j][step] + k[i][j][0]/6 + k[i][j][1]/3 + k[i][j][3]/3 + k[i][j]/6;
+			v[i][j][step + 1] = v[i][j][step] + w[i][j][0]/6 + w[i][j][1]/3 + w[i][j][3]/3 + w[i][j]/6;
+		}
+	}
+	return x, v;
+
 
 
 int main(void)
