87f6ada65c76915c77e0023d686a3d789fd889dc
Bastian Krones <bkrones.uni@gmail.com>
2018-01-21 20:42:30 +0100

    Added more tests

--- simulation/test/input_physical_data_TEST.c
+++ simulation/test/input_physical_data_TEST.c
@@ -1,6 +1,13 @@
 #include "../input_physical_data.h"
 #include "input_physical_data_TEST.h"
 
+// double initial_velocities(int n, int d)
+// {
+// 	const int initial_velocities[N][D] = {{0, 0},
+// 										  {0, 0},
+// 										  {0, 0}};
+// 	return initial_velocities[n][d];
+// }
 short initial_velocities_TEST()
 {
     if (initial_velocities(1, 1) == 0)
@@ -13,6 +20,13 @@ short initial_velocities_TEST()
     }
 }
 
+// double initial_masses(int n)
+// {
+// 	const int initial_masses[N] = {1,
+// 								   1,
+// 								   1};
+// 	return initial_masses[n];
+// }
 short initial_masses_TEST()
 {
     if (initial_masses(1) == 1)
@@ -25,7 +39,13 @@ short initial_masses_TEST()
     }
 }
 
-// InitialIposition of all bodies (first entry = Saturn, second = moon 1, third = moon 2)
+// double initial_pos(int b, int z)
+// {
+// 	const double initial_positions[N][D] = {{0, 0},
+// 											{1, 1},
+// 											{1, -1}};
+// 	return initial_positions[b][z];
+// }
 short initial_pos_TEST()
 {
     if (initial_pos(2, 1) == -1)
