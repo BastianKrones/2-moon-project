296b914135b6cc56e5b147f47ba13f11cc4ce937
Bastian Krones <bkrones.uni@gmail.com>
2018-01-21 20:38:53 +0100

    Tests included

--- simulation/test/input_physical_data_TEST.c
+++ simulation/test/input_physical_data_TEST.c
@@ -1,9 +1,34 @@
 #include "../input_physical_data.h"
 #include "input_physical_data_TEST.h"
 
-short initial_velocities_TEST(int n, int d)
+short initial_velocities_TEST()
 {
-	if(initial_velocities(1,1) == {0,0})
+    if (initial_velocities(1, 1) == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+short initial_masses_TEST()
+{
+    if (initial_masses(1) == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// InitialIposition of all bodies (first entry = Saturn, second = moon 1, third = moon 2)
+short initial_pos_TEST()
+{
+    if (initial_pos(2, 1) == -1)
     {
         return 1;
     }
@@ -14,7 +39,7 @@ short initial_velocities_TEST(int n, int d)
 }
 
 // Mass of all bodies
-short initial_masses_TEST(int n)
+/* short initial_masses_TEST(int n)
 {
 	if(h() == 0.5)
     {
@@ -36,4 +61,4 @@ short initial_pos_TEST(int b, int z)
     {
         return 0;
     }
-}
+} */
--- simulation/test/main_TEST.c
+++ simulation/test/main_TEST.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "input_parameters_TEST.h"
+#include "input_physical_data_TEST.h"
 
 void main()
 {
     printf("h()-Function: %i \n", h_TEST());
     printf("size()-Function: %i \n", h_TEST());
+    printf("initial_velocities_TEST-Function: %i \n", initial_velocities_TEST());
+    printf("initial_masses_TEST-Function: %i \n", initial_masses_TEST());
+    printf("initial_pos_TEST-Function: %i \n", initial_pos_TEST());
 }
