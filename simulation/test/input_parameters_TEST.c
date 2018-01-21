cfd326b64b6904201989675b2207544c635ea87a
Bastian Krones <bkrones.uni@gmail.com>
2018-01-21 22:15:29 +0100

    Fine-tuned tests and colorized them

--- simulation/test/input_parameters_TEST.c
+++ simulation/test/input_parameters_TEST.c
@@ -3,10 +3,10 @@
 
 short h_TEST()
 {
-    (h() == 0.5) ? 1 : 0;
+    return (h() == 0.5) ? 1 : 0;
 }
 
 short size_TEST()
 {
-    (h() == 40) ? 1 : 0;
+    return (h() == 40) ? 1 : 0;
 }
--- simulation/test/main_TEST.c
+++ simulation/test/main_TEST.c
@@ -2,11 +2,21 @@
 #include "input_parameters_TEST.h"
 #include "input_physical_data_TEST.h"
 
+#define ANSI_COLOR_RED          "\033[1m\033[31m"
+#define ANSI_COLOR_GREEN        "\x1b[32m"
+#define ANSI_COLOR_BOLD_RED     "\x1b[31m"
+#define ANSI_COLOR_BOLD_GREEN   "\033[1m\033[32m"
+#define ANSI_COLOR_WHITE        "\x1b[0;37m"
+
 void print_test(char [], short);
 
 void main()
 {
-    printf("Testing functions: \n");
+    printf("\n#####################\n");
+    printf("#                   #\n");
+    printf("# Testing functions #\n");
+    printf("#                   #\n");
+    printf("#####################\n\n");
     print_test("h", h_TEST());
     print_test("size", h_TEST());
     print_test("initial_velocities", initial_velocities_TEST());
@@ -16,6 +26,15 @@ void main()
 
 void print_test(char func_name[], short return_value)
 {
-    printf("%s: %s \n", (return_value==1) ? "SUCCESS" : "FAILED ", func_name);
+    printf(ANSI_COLOR_WHITE "[ ");
+    if (return_value == 1)
+    {
+        printf(ANSI_COLOR_BOLD_GREEN "%s", "\xE2\x9C\x93");
+    }
+    else
+    {
+        printf(ANSI_COLOR_BOLD_RED "%s", "X");
+    }
+    printf(ANSI_COLOR_WHITE " ]: %s \n", func_name);
     return;
-}-.
+}
