4700d8641307e89d7aaea23d54a287bee6cbbf56
Bastian Krones <bkrones.uni@gmail.com>
2018-01-27 12:10:21 +0100

    Uhm made some changes i think

--- simulation/initialize.h
+++ simulation/initialize.h
@@ -1,9 +1,8 @@
-#include "cmd_args.h"
 #include "input_parameters.h"
 
 #ifndef INITIALIZEN
 #define INITIALIZEN
 
-void initialize();
+void initialize(double energy, double t, double x[N][D], double v[N][D], double m[N]);
 
 #endif
