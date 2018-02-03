680cf73b09b5421f32b12ab85cf3b1967de39ecf
Bomel <keiwanjamaly@hotmail.de>
2018-02-03 17:10:13 +0100

    changes

--- simulation/ejs.c
+++ simulation/ejs.c
@@ -5,13 +5,12 @@
 #include "./check_simulation/energy.h"
 #include "./runge_kutta/next.h"
 
-
 int main(int argc, char *argv[])
 {
     FILE *fp;
 
     fp = fopen("./output_coord_cartesian", "w");
-    
+
     fclose(fp);
 
     // define everything
@@ -25,27 +24,45 @@ int main(int argc, char *argv[])
 
     t = 0;
 
-    long double t_end = 10 * 365 * 24 * 60 * 60;
-    save_data_to_file(t, x, v);
+    long double t_end = 20 * 365 * 24 * 60 * 60;
 
+    fp = fopen("./output_coord_cartesian", "a");
     // working loop remember t = t_0
     int u = 0;
     while (t <= t_end)
     {
-        // Calculated step
-        next(x, v, &t, m);
-
-        // show a progress and save the data to the file every 300 steps
-
-        if (u%1000 == 0)
+        // save_data_to_file(t, x, v);
+        if (u % 1 == 0)
         {
 
-            save_data_to_file(t, x, v);
-            printf("\r                                    ");
-            printf("\r%4.2Lf\n", t/(t_end) * 100);
+            printf("%4.2Lf\n", t / (t_end)*100);
+
+            // fprintf(fp, "%50.4Lf", t);
+
+            fprintf(fp, "%20.4Lf", t);
+
+            for (int i = N-1; i >= 0; i--)
+            {
+                for (int j = D-1; j >= 0; j--)
+                {
+                    fprintf(fp, "%20.4Lf", x[i][j]);
+                }
+            }
+
+            long double p = calculate_distance(1, 2, x);
+
+            fprintf(fp, "%20.4Lf", p);
+
+            fprintf(fp, "\n");
         }
+
         u++;
+
+        // Calculated step
+        next(x, v, &t, m);
     }
+
+    fclose(fp);
 
     // calculate_kin_energy(1, v, m);
 
